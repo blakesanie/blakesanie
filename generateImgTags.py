1f5e5e9fe482736de17fffae8562e9aea26bc032
Blake Sanie <blake@sanie.com>
2020-11-27 14:07:00 -0800

    icon paths updated for react site

--- generateImgTags.py
+++ generateImgTags.py
@@ -1,7 +1,7 @@
 import os
 
-basePath = "../blakesanie.com/cs/techUsed"
+basePath = "../blakesanie.com/build/images/cs/techUsed"
 
 for filename in os.listdir(basePath):
     if filename != ".DS_Store":
-        print('<img height="20" style="margin-bottom: 10px;" src="https://blakesanie.com/cs/techUsed/{}" />&nbsp;&nbsp;'.format(filename))
+        print('<img height="20" style="margin-bottom: 10px;" src="https://blakesanie.com/images/cs/techUsed/{}" />&nbsp;&nbsp;'.format(filename))
