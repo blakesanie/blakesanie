8d1e0402274ff4c8a402f77094cbd987291c7965
Blake Sanie <blake@sanie.com>
2021-11-30 00:52:11 -0500

    sort correctly (date desc), remove twitter

--- feed.py
+++ feed.py
@@ -1,14 +1,17 @@
 import feedparser
+import time
 
 NewsFeed = feedparser.parse(
     "https://medium.com/feed/@blakesanie")
 
-articles = sorted(NewsFeed.entries, key=lambda x: x['published_parsed'])[:5]
+articles = sorted(NewsFeed.entries,
+                  key=lambda x: x['published_parsed'], reverse=True)[:5]
 
 html = ""
 
 for article in articles:
-    html += f"<li><a href='{article['link']} target='_blank'>{article['title']}</a></li>"
+    print(article)
+    html += f"<li><a href='{article['link']} target='_blank'>{article['title']} - {time.strftime('%m/%d/%Y', article['published_parsed'])}</a></li>"
 
 html = '<ul>' + html + '</ul>'
 
