9cf5e32ea3983d2191a0e35b6195ad536ad351a8
Blake Sanie <38799390+blakesanie@users.noreply.github.com>
2021-11-30 00:44:28 -0500

    Delete generateImgTags.py

--- generateImgTags.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basePath = "../blakesanie.com/build/images/cs/techUsed"
-
-for filename in os.listdir(basePath):
-    if filename != ".DS_Store":
-        print('<img height="20" style="margin-bottom: 10px;" src="https://blakesanie.com/images/cs/techUsed/{}" />&nbsp;&nbsp;'.format(filename))
